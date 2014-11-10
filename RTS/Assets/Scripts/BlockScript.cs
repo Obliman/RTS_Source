087c6d01be65c94eee5bfa35074079a1b99819cb
Obliman <nato5342@hotmail.com>
2014-11-10 10:48:27 -0600

    Prototype
    11/10/14 @ 10:48 AM

---
+++ RTS/Assets/Scripts/BlockScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockScript : MonoBehaviour {
+
+	float X;
+	float Y;
+	float Z;
+
+	// Use this for initialization
+	void Start () {
+		X = Mathf.Round (gameObject.transform.position.x*0.5f)/0.5f;
+		Y = 1.0f+Mathf.Round (gameObject.transform.position.y*0.5f)/0.5f;
+		Z = Mathf.Round (gameObject.transform.position.z*0.5f)/0.5f;
+		gameObject.transform.position = new Vector3 (X,Y,Z);
+	}
+
+	void FixedUpdate () {
+	
+	}
+}
---
+++ RTS/Assets/Scripts/GhostBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostBlock : MonoBehaviour {
+
+	RaycastHit hit;
+	public GameObject Block;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)){
+			gameObject.transform.position = new Vector3(Mathf.Round(hit.point.x*0.5f)/0.5f,1.0f+Mathf.Round(hit.point.y*0.5f)/0.5f,Mathf.Round(hit.point.z*0.5f)/0.5f);
+			if(Input.GetMouseButtonDown(0)){
+				Instantiate(Block,new Vector3(transform.position.x,transform.position.y-1.0f,transform.position.z),transform.localRotation);
+				//Destroy (this.gameObject);
+			}
+		}
+	}
+}
